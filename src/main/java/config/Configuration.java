ce1ed0803a4a5098be88a918810b152e1a0a59c9
Owen Hallock <owenhallock29@gmail.com>
2025-02-03 21:13:00 -0500

    quick upd8 to config

--- src/main/java/config/Configuration.java
+++ src/main/java/config/Configuration.java
@@ -3,7 +3,7 @@ package config;
 import structures.BinarySearchTree;
 import structures.BinaryTreeNode;
 import structures.BinaryTreeUtility;
-
+import structures.MyBinaryTreeNode;
 
 /**
  * This class acts as a configuration file which tells the testing framework
@@ -12,9 +12,10 @@ import structures.BinaryTreeUtility;
  */
 public class Configuration {
 	public static <T> BinaryTreeNode<T> createBinaryTreeNode(BinaryTreeNode<T> left, T elem, BinaryTreeNode<T> right){
-		return null;
+		return new MyBinaryTreeNode<>(left, elem, right);
 	}
-	
+
+//merge other stuffz to master brancH?
 	public static BinaryTreeUtility createBinaryTreeUtility(){
 		return null;
 	}
